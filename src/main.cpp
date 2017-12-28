47764b053b05377b329099863f70a10a49f46385
pacificao <nathan@pacificao.com>
2017-12-28 04:48:48 +0000

    rc1.0.0.1

--- src/main.cpp
+++ src/main.cpp
@@ -1000,7 +1000,7 @@ uint256 WantedByOrphan(const CBlock* pblockOrphan)
 // miner's coin base reward
 int64_t GetProofOfWorkReward(int64_t nFees)
 {
-    int64_t nSubsidy = 1500 * COIN;
+    int64_t nSubsidy = 90 * COIN;
 
     if (fDebug && GetBoolArg("-printcreation"))
         printf("GetProofOfWorkReward() : create=%s nSubsidy=%"PRId64"\n", FormatMoney(nSubsidy).c_str(), nSubsidy);
